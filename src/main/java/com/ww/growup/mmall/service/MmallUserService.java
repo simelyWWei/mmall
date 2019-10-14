03767bf0dc019efa6afefa161d3cb3b62ba11007
weiwei <weiw26@chinaunicom.cn>
2019-10-14 19:39:15 +0800

    添加自定义自动配置

--- src/main/java/com/ww/growup/mmall/service/MmallUserService.java
+++ src/main/java/com/ww/growup/mmall/service/MmallUserService.java
@@ -1,9 +1,11 @@
 package com.ww.growup.mmall.service;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ww.growup.autoconfig.demo.utils.encrypt.MD5Util;
 import com.ww.growup.mmall.entity.MmallUser;
 import com.ww.growup.mmall.mapper.MmallUserMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -13,9 +15,15 @@ import java.util.List;
 @Service
 public class MmallUserService extends ServiceImpl<MmallUserMapper, MmallUser> {
 
+    @Autowired
+    private MD5Util md5Util;
+
     @PostConstruct
     void initTest() {
         List<MmallUser> list = this.list();
         list.stream().forEach(e -> log.info(e.getUsername()));
+
+        String testvar = "123";
+        log.info("test MD5 inject: {}",md5Util.encode(testvar).toUpperCase());
     }
 }
